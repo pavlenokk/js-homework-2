8a7bf47720e57d50bbea8759ed920647d8b704a2
Pavlenok <pavlenok15@gmail.com>
2022-07-23 12:23:03 +0300

    Initial commit

---
+++ .idea/js_1/jscript.js
@@ -0,0 +1,66 @@
+let str = 'hello '
+console.log(str)
+
+let str1 = 'owu.'
+console.log(str1)
+
+let str2 = 'com'
+console.log(str2)
+
+let str3 = '.ua'
+console.log(str3)
+
+let a = 1
+console.log(a)
+
+let a1 = 10
+console.log(a1)
+
+let a2 = -999
+console.log(a2)
+
+let a3 =123
+console.log(a3)
+
+let a4 = 3.14
+console.log(a4)
+
+let a5 = 2.7
+console.log(a5)
+
+let a6 = 16
+console.log(a6)
+
+let bool1 = true
+console.log(bool1)
+
+let bool2 = false
+console.log(bool2)
+
+let str4 = 'Andrew '
+let str5 = 'Sergiyovych '
+let str6 = 'Pavlenok'
+
+let person = str4+str5+str6
+console.log(person)
+
+let a7 = 100
+let b = '100'
+let c = false
+console.log(typeof a7,b,c)
+
+let name = prompt('Как тебя зовут?',);
+alert(`Нет ${name} , ты Славик!`); //
+console.log(name)
+
+let mood = prompt('Шо ты там?',);
+alert(`Это ${mood} настроение!`);
+console.log(mood)
+
+let age = prompt('Сколько тебе лет, молодой?', 100);
+alert(`Тебе ${age} лет!`); // Тебе 100 лет!
+console.log(age)
+
+
+
+
