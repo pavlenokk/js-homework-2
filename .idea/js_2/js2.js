4e4059b8bbf5a8d369266bdc65b48107fccb192d
pavlenokk <pavlenok15@gmail.com>
2022-07-27 12:54:38 +0300

    123

---
+++ .idea/js_2/js2.js
@@ -0,0 +1,135 @@
+let first = [`what`, `are`, `you`, `doing`, `here`, 12, 12, 2001,true, false]
+console.log(first)
+
+console.log(first[0])
+console.log(first[1])
+console.log(first[2])
+console.log(first[3])
+console.log(first[4])
+console.log(first[5])
+console.log(first[6])
+console.log(first[7])
+console.log(first[8])
+console.log(first[9])
+
+let firstbook = {
+    title: `1984`,
+    pagecount: 543,
+    genre: `antiutophian`
+}
+
+let secondbook = {
+    title: `1944`,
+    pagecount: 562,
+    genre: `drama`
+}
+
+let thirdbook = {
+    title: `2012`,
+    pagecount: 543,
+    genre: `horror`
+}
+let books = [firstbook, secondbook, thirdbook]
+console.log(books)
+
+let book1 = {
+    title: `Harryk Poterenko`,
+    pagecount: 5321,
+    genre: `poem`,
+
+    author: {
+        name: `Stepan`,
+        age: 57
+    }
+}
+
+let book2 = {
+    title: `Leonid Dicaprenko`,
+    pagecount: 752,
+    genre: `classic`,
+
+    author: {
+        name: `Mike`,
+        age: 12
+    }
+}
+
+let book3 = {
+    title: `Petro Mostavchuk`,
+    pagecount: 126,
+    genre: `hardbass`,
+
+    author: {
+        name: `Ivan`,
+        age: 64
+    }
+}
+
+let libraly = [book1 , book2, book3]
+console.log(libraly)
+
+
+let user = {
+    name: `Misha`,
+    username:`DJ`,
+    password: 12
+}
+console.log(user.password)
+let user1 = {
+    name: `Misha`,
+    username:`DJ`,
+    password: 21
+}
+console.log(user1.password)
+let user2 = {
+    name: `Misha`,
+    username:`DJ`,
+    password: 32
+}
+console.log(user2.password)
+let user3 = {
+    name: `Misha`,
+    username:`DJ`,
+    password: 54
+}
+console.log(user3.password)
+let user4 = {
+    name: `Misha`,
+    username:`DJ`,
+    password: 4214
+
+}
+console.log(user4.password)
+let user5 = {
+    name: `Misha`,
+    username:`DJ`,
+    password: 21321
+}
+console.log(user5.password)
+let user6 = {
+    name: `Misha`,
+    username:`DJ`,
+    password: 65464
+}
+console.log(user6.password)
+let user7 = {
+    name: `Misha`,
+    username:`DJ`,
+    password: 32532
+}
+console.log(user7.password)
+let user8 = {
+    name: `Misha`,
+    username:`DJ`,
+    password: 423523
+}
+console.log(user8.password)
+let user9 = {
+    name: `Misha`,
+    username: `DJ`,
+    password: 532532
+}
+console.log(user9.password)
+
+//let userspassword = [user.password,user1.password,user2.password,user3.password,user4.password,user5.password,user6.password,user7.password,user8.password,user9.password]
+//console.log(userspassword)
